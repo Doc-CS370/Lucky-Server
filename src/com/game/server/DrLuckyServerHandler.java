9b215ae8102d85b1e533631eee394fe31b105d97
rehmano <omar@syed.dev>
2019-10-24 11:37:17 -0400

    Implemented game starting aswell as handling new players joining.

--- src/com/game/server/DrLuckyServerHandler.java
+++ src/com/game/server/DrLuckyServerHandler.java
@@ -11,12 +11,27 @@ import io.netty.util.concurrent.GenericFutureListener;
 import io.netty.util.concurrent.GlobalEventExecutor;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * "endturn"
+ * "good"
+ * "endgame"
+ * "newplayer"
+ * "
+ *
+ */
 
 
 public class DrLuckyServerHandler extends SimpleChannelInboundHandler<String> {
     static final ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
     static ArrayList<Channel> connectedPlayers = new ArrayList<>();
+    static int desiredPlayers = 0;
     static Channel currentPlayer = null;
+    static Map<Channel, Set<Integer>> playerPositions; //Map the channel to their coordinates
+    static int currentTurn = 0;
+    static boolean gameHasStarted = false;
     static int playerNum = 0;
 
     @Override
@@ -31,8 +46,16 @@ public class DrLuckyServerHandler extends SimpleChannelInboundHandler<String> {
                     if(currentPlayer == null){
                         currentPlayer = ctx.channel();
                     }
+
                     connectedPlayers.add(ctx.channel());
                     channels.add(ctx.channel());
+                    if(channels.size() > 1){
+                        for(Channel ch: channels){
+                            if(ch != ctx.channel()){
+                                ch.writeAndFlush("newplayer");
+                            }
+                        }
+                    }
                 });
     }
 
@@ -40,14 +63,43 @@ public class DrLuckyServerHandler extends SimpleChannelInboundHandler<String> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, String msg){
         System.out.println("User has said something: " + msg);
-        //Loop through all connected channels
-        for (Channel c: channels) {
-            if (c != ctx.channel()) { //send to all other channels that isnt the one who sent the message
-                c.writeAndFlush("[" + ctx.channel().remoteAddress() + "] " + msg + '\n');
-            } else {
-                //If its the channel that sent the message.
-                c.writeAndFlush("[you] " + msg + '\n');
+        if(!gameHasStarted){ //Handle when game has not started
+            desiredPlayers = Integer.parseInt(msg); //First message should be first player saying how many players they want.
+
+        }
+        if(gameHasStarted){ //Make sure game has started
+            if(isCurrentPlayer(ctx.channel())){ //Make sure its the current player
+                if(msg.equalsIgnoreCase("endturn")){
+                    endTurn();
+                }
+                for (Channel c: channels) {
+                    if (c != ctx.channel()) { //send to all other channels that isnt the one who sent the message
+
+                    } else {
+                        //If its the channel that sent the message.
+
+                    }
+                }
             }
         }
+
+    }
+
+    public boolean canGameStart(){
+        return connectedPlayers.size() != 0 && connectedPlayers.size() == desiredPlayers;
+    }
+
+    public void endTurn(){ //Cycle to the next player if the current player has issued the command
+        if(playerNum != connectedPlayers.size() - 1){
+            currentPlayer = connectedPlayers.get(playerNum);
+            playerNum++;
+        } else {
+            currentPlayer = connectedPlayers.get(0);
+            playerNum = 0;
+        }
+    }
+
+    public boolean isCurrentPlayer(Channel ch){
+        return ch == currentPlayer;
     }
 }
